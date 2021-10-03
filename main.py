84d48e7176dd57d4bf0e367eb8ce71638338ca02
yksen <selegakamil@gmail.com>
2021-10-03 23:09:13 +0200

    Bouncy square actually, also changing the gravity upon hitting sides

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import sys
 pygame.init()
 
 WINDOW_WIDTH = 1000
-WINDOW_HEIGHT = 1000
+WINDOW_HEIGHT = 1080
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Game2")
 
@@ -20,11 +20,11 @@ FRAME_TIME = pygame.time.Clock()
 # VARIABLES #
 
 dt = 0.001
-gravity = 10
+gravity = 5
 
 player_x = 500
 player_y = 500
-player_width = 100
+player_width = 50
 player_height = player_width
 player_velocity_x = 0
 player_velocity_y = 0
@@ -40,10 +40,7 @@ while True:
     # EVENTS #
     
     events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    for event in events:        
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not mouse_clicked:
             mouse_clicked = True
             mouse_start_position = pygame.mouse.get_pos()
@@ -52,13 +49,18 @@ while True:
             mouse_end_position = pygame.mouse.get_pos()
             player_velocity_x += (mouse_start_position[0] - mouse_end_position[0]) * 0.01
             player_velocity_y += (mouse_start_position[1] - mouse_end_position[1]) * 0.01
+        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.constants.K_ESCAPE:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN and event.key == pygame.constants.K_v:
+            gravity *= -1
 
     # DRAWING #
 
     WINDOW.fill((0, 0, 0))
     draw_centered_rectangle(player_x, player_y, player_width, player_height, (150, 27, 95), WINDOW)
     pygame.display.update()
-
+    
     # PHYSICS #
 
     player_x += player_velocity_x
@@ -69,12 +71,13 @@ while True:
     if player_x + player_velocity_x < player_width / 2 or player_x + player_velocity_x > WINDOW_WIDTH - player_width / 2:
         player_velocity_x += (player_velocity_x / 2) * (-1)
         player_velocity_x *= -1
+        gravity *= -1
     if player_y + player_velocity_y < player_height / 2 or player_y + player_velocity_y > WINDOW_HEIGHT - player_height / 2:
         player_velocity_x += (player_velocity_x / 2) * (-1)
         player_velocity_y -= player_velocity_y / 2
         player_velocity_y *= -1
-    
-    # TIME #
 
+    # TIME #
+    
     FRAME_TIME.tick()
     dt = 0.001 * FRAME_TIME.get_time()
