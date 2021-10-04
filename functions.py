56f59c7dd41e45c70540391287b1008ab116d603
yksen <selegakamil@gmail.com>
2021-10-04 18:56:03 +0200

    Working game first prototype

--- functions.py
+++ functions.py
@@ -1,5 +1,90 @@
 import pygame
+import random
 
-def draw_centered_rectangle(x, y, width, height, color, window):
-    pygame.draw.rect(window, color, pygame.Rect(x - width / 2, y - height / 2, width, height))
+# ESSENTIALS #
 
+pygame.init()
+
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 1080
+
+MAIN_FONT = pygame.font.SysFont('calibri', int(WINDOW_HEIGHT / 16), True)
+MAIN_FONT_SMALL = pygame.font.SysFont('calibri', int(WINDOW_HEIGHT / 32), italic=True)
+
+
+# VARIABLES #
+
+relative_height = 0
+
+platform_minimum_width = 250
+platform_maximum_width = 400
+platform_height = 25
+
+class Platform:
+    def __init__(self, id, x, y, width, height):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+def generate_platform(id):
+    return Platform(
+        id,
+        random.randint(platform_maximum_width / 2, WINDOW_WIDTH - platform_maximum_width / 2),
+        WINDOW_HEIGHT / 4 + id * (WINDOW_HEIGHT / 2),
+        random.randint(platform_minimum_width, platform_maximum_width),
+        platform_height
+    )
+
+def player_rectangle(x, y, width, height):
+    global relative_height
+    return pygame.Rect(x - width / 2, WINDOW_HEIGHT - y - height / 2 + relative_height, width, height)
+
+def platform_rectangle(platform):
+    global relative_height
+    return pygame.Rect(platform.x - platform.width / 2, WINDOW_HEIGHT - platform.y - platform.height / 2 + relative_height, platform.width, platform.height)
+
+def draw_rectangle(window, color, rect):
+    pygame.draw.rect(window, color, rect)
+
+def draw_platform(window, rect):
+    draw_rectangle(window, (255, 255, 255), rect)
+
+def draw_score(window, score):
+    text = MAIN_FONT.render(str(score), True, (24, 158, 199))
+    window.blit(text, text.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)))
+
+def draw_death_message(window, score):
+    text = MAIN_FONT_SMALL.render("you died, your final score is " + str(score), True, (24, 158, 199))
+    text2 = MAIN_FONT_SMALL.render("press right mouse button to reset", True, (24, 158, 199))
+    window.blit(text, text.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - MAIN_FONT_SMALL.size("you died, your final score is ")[1] / 2)))
+    window.blit(text2, text2.get_rect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 + MAIN_FONT_SMALL.size("press right mouse button to reset")[1] / 2)))
+
+def horizontal_bounce(player_velocity_x):
+    player_velocity_x += (player_velocity_x / 2) * (-1)
+    player_velocity_x *= -1
+    return player_velocity_x
+
+def vertical_bounce(player_velocity_x, player_velocity_y):
+    player_velocity_x += (player_velocity_x / 2) * (-1)
+    player_velocity_y -= player_velocity_y / 2
+    player_velocity_y *= -1
+    return player_velocity_x, player_velocity_y
+
+def determine_side(rect1, rect2):
+    if rect2.right > rect1.left and rect2.left < rect1.right:
+        if rect2.bottom < rect1.center[1]:
+            return "vertical", "top"
+        else:
+            return "vertical", "bottom"
+    else:
+        return "horizontal"
+
+def change_relative_height(height):
+    global relative_height
+    if height == 'reset':
+        relative_height = 0
+    else:
+        relative_height += height
+    return relative_height
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from functions import *
 import pygame
 import pygame.constants
-import random
 import sys
 
 # ESSENTIALS #
@@ -10,20 +9,22 @@ pygame.init()
 
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 1080
-WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.NOFRAME)
 pygame.display.set_caption("Game2")
-
-MAIN_FONT =  pygame.font.SysFont('calibri', 24)
 
 FRAME_TIME = pygame.time.Clock()
 
 # VARIABLES #
 
 dt = 0.001
-gravity = 5
+gravity = -5
+
+screen_scrolling_active = False
+relative_height = 0
+height_increase = 1
 
 player_x = 500
-player_y = 500
+player_y = 100
 player_width = 50
 player_height = player_width
 player_velocity_x = 0
@@ -33,6 +34,16 @@ mouse_clicked = False
 mouse_start_position = (0, 0)
 mouse_end_position = (0, 0)
 
+score = 0
+player_alive = True
+death_enabled = False
+
+platform_id = 0
+platforms = []
+platforms.append(generate_platform(platform_id))
+platform_id += 1
+platforms.append(generate_platform(platform_id))
+
 # GAME LOOP #
 
 while True:
@@ -41,41 +52,96 @@ while True:
     
     events = pygame.event.get()
     for event in events:        
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not mouse_clicked:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not mouse_clicked and player_alive:
             mouse_clicked = True
             mouse_start_position = pygame.mouse.get_pos()
-        if event.type == pygame.MOUSEBUTTONUP and event.button == 1 and mouse_clicked:
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 1 and mouse_clicked and player_alive:
             mouse_clicked = False
             mouse_end_position = pygame.mouse.get_pos()
             player_velocity_x += (mouse_start_position[0] - mouse_end_position[0]) * 0.01
-            player_velocity_y += (mouse_start_position[1] - mouse_end_position[1]) * 0.01
+            player_velocity_y -= (mouse_start_position[1] - mouse_end_position[1]) * 0.01
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 3:
+            screen_scrolling_active = False
+            relative_height = change_relative_height("reset")
+            height_increase = 1
+            
+            player_x = 500
+            player_y = 100
+            player_velocity_x = 0
+            player_velocity_y = 0
+            platforms.clear()
+            score = 0
+            player_alive = True
+            death_enabled = False
+
+            platform_id = 0
+            platforms = []
+            platforms.append(generate_platform(platform_id))
+            platform_id += 1
+            platforms.append(generate_platform(platform_id))
         if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.constants.K_ESCAPE:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.KEYDOWN and event.key == pygame.constants.K_v:
-            gravity *= -1
 
     # DRAWING #
 
     WINDOW.fill((0, 0, 0))
-    draw_centered_rectangle(player_x, player_y, player_width, player_height, (150, 27, 95), WINDOW)
+
+    if screen_scrolling_active and relative_height < (WINDOW_HEIGHT / 2) * (platform_id - 1):        
+        relative_height = change_relative_height(height_increase)
+        if relative_height + height_increase >= (WINDOW_HEIGHT / 2) * (platform_id - 1):
+            screen_scrolling_active = False
+
+    if player_alive:
+        draw_score(WINDOW, score)
+    else:
+        draw_death_message(WINDOW, score)
+        
+    draw_rectangle(WINDOW, (150, 27, 95), player_rectangle(player_x, player_y, player_width, player_height))
+    for platform in platforms:
+        draw_platform(WINDOW, platform_rectangle(platform))
+
     pygame.display.update()
-    
+
+    # COLLISIONS #
+
+    if player_x + player_velocity_x < player_width / 2 or player_x + player_velocity_x > WINDOW_WIDTH - player_width / 2:
+        player_velocity_x = horizontal_bounce(player_velocity_x)
+    if player_y + player_velocity_y < player_height / 2:
+        if death_enabled:
+            player_alive = False
+        else:
+            player_velocity_x, player_velocity_y = vertical_bounce(player_velocity_x, player_velocity_y)
+    elif player_y + player_velocity_y > WINDOW_HEIGHT - player_height / 2 + relative_height:
+        player_velocity_x, player_velocity_y = vertical_bounce(player_velocity_x, player_velocity_y)
+    for platform in platforms:
+        player_rect = player_rectangle(player_x + player_velocity_x, player_y + player_velocity_y, player_width, player_height)
+        platform_rect = platform_rectangle(platform)
+        if pygame.Rect.colliderect(player_rect, platform_rect):
+            player_rect = player_rectangle(player_x, player_y, player_width, player_height)
+            collision_side = determine_side(platform_rect, player_rect)
+            if collision_side[0] == "vertical":
+                player_velocity_x, player_velocity_y = vertical_bounce(player_velocity_x, player_velocity_y)
+                if collision_side[1] == "top" and platform.id + 1 > score:
+                    score = platform.id + 1
+            else:
+                player_velocity_x = horizontal_bounce(player_velocity_x)
+
     # PHYSICS #
 
     player_x += player_velocity_x
     player_y += player_velocity_y
-
     player_velocity_y += gravity * dt
 
-    if player_x + player_velocity_x < player_width / 2 or player_x + player_velocity_x > WINDOW_WIDTH - player_width / 2:
-        player_velocity_x += (player_velocity_x / 2) * (-1)
-        player_velocity_x *= -1
-        gravity *= -1
-    if player_y + player_velocity_y < player_height / 2 or player_y + player_velocity_y > WINDOW_HEIGHT - player_height / 2:
-        player_velocity_x += (player_velocity_x / 2) * (-1)
-        player_velocity_y -= player_velocity_y / 2
-        player_velocity_y *= -1
+    # GAME #
+
+    if score == platform_id + 1:
+        if score == 2:
+            death_enabled = True
+        platform_id += 1
+        platforms.append(generate_platform(platform_id))
+        screen_scrolling_active = True
+        platforms.pop(0)
 
     # TIME #
     
